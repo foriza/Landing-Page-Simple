eb28433ae999b310decb423217d7ce14efa90e24
foriza <87646360+foriza@users.noreply.github.com>
2021-07-25 18:54:41 +0000

    Deploying to gh-pages from @ foriza/Landing-Page-Simple@9e7a468debd057e806873f445ece531a461d3079 🚀

---
+++ _next/static/C1kMXqdBWw3X3rZaxDtaF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7420e087b9674f9a72ce.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
